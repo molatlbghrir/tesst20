d1445e11ee8b0c0b0653651d595581be3aca5669
kawtar <test@gmail.com>
2020-02-03 23:55:16 +0100

    Initialization

---
+++ srrrrr/srrrrr/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace srrrrr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
